9149d9d7bf8ee4147454f2412655b54294911d12
headlulu888 <headlulu888@gmail.com>
2017-07-06 21:28:46 +0300

    it 'correct .correct_answer_key' do

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -7,15 +7,25 @@ RSpec.describe GameQuestion, type: :model do
   context 'game status' do
 
     it 'correct .variants' do
-      expect(game_question.variants).to eq({'a' => game_question.question.answer2,
-                                            'b' => game_question.question.answer1,
-                                            'c' => game_question.question.answer4,
-                                            'd' => game_question.question.answer3,
-                                            })
+      expect(game_question.variants).to eq(
+        {'a' => game_question.question.answer2,
+         'b' => game_question.question.answer1,
+         'c' => game_question.question.answer4,
+         'd' => game_question.question.answer3,
+         })
     end
 
     it 'correct .answer_correct?' do
       expect(game_question.answer_correct?('b')).to be_truthy
+    end
+
+    it 'correct .level & .text delegates' do
+      expect(game_question.text).to eq(game_question.question.text)
+      expect(game_question.level).to eq(game_question.question.level)
+    end
+    # to_do
+    it 'correct .correct_answer_key' do
+      expect(game_question.correct_answer_key).to eq 'b'
     end
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'support/my_spec_helper'
 
 RSpec.describe Game, type: :model do
-  # пользователь для создания игр 
+  # пользователь для создания игр
   let(:user) { FactoryGirl.create(:user) }
   # игра с прописанными игровыми вопросами
   let(:game_w_questions) { FactoryGirl.create(:game_with_questions, user: user) }
@@ -21,7 +21,7 @@ RSpec.describe Game, type: :model do
         expect(game.game_questions.size).to eq(15)
         expect(game.game_questions.map(&:level)).to eq (0..14).to_a
     end
-  end 
+  end
 
 
   context 'game mechanics' do
@@ -39,6 +39,50 @@ RSpec.describe Game, type: :model do
 
       expect(game_w_questions.status).to eq(:in_progress)
       expect(game_w_questions.finished?).to be_falsey
+    end
+  end
+
+  context 'take money!' do
+    it 'take_money! finishes the game' do
+      q = game_w_questions.current_game_question
+      game_w_questions.answer_current_question!(q.correct_answer_key)
+
+      game_w_questions.take_money!
+
+      prize = game_w_questions.prize
+      expect(prize).to be > 0
+
+      expect(game_w_questions.status).to eq :money
+      expect(game_w_questions.finished?).to be_truthy
+      expect(user.balance).to eq prize
+    end
+  end
+
+  context '.status' do
+  # перед каждым тестом "завершаем игру"
+    before(:each) do
+      game_w_questions.finished_at = Time.now
+      expect(game_w_questions.finished?).to be_truthy
+    end
+
+    it ':won' do
+      game_w_questions.current_level = Question::QUESTION_LEVELS.max + 1
+      expect(game_w_questions.status).to eq(:won)
+    end
+
+    it ':fail' do
+      game_w_questions.is_failed = true
+      expect(game_w_questions.status).to eq(:fail)
+    end
+
+    it ':timeout' do
+      game_w_questions.created_at = 1.hour.ago
+      game_w_questions.is_failed = true
+      expect(game_w_questions.status).to eq(:timeout)
+    end
+
+    it ':money' do
+      expect(game_w_questions.status).to eq(:money)
     end
   end
 end
