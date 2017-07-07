825ccd1760c1a20d56950c667df74c8d4419a3d1
headlulu888 <headlulu888@gmail.com>
2017-07-07 13:18:38 +0300

    Added test wrong player response

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -95,6 +95,16 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(game_path(game_w_questions))
       expect(flash[:alert]).to be
     end
+
+    it 'wrong player response' do
+      put :answer, id: game_w_questions.id, letter: 'a'
+      game = assigns(:game)
+
+      expect(game.finished?).to be_truthy
+      expect(game.prize).to eq 0
+      expect(response).to redirect_to(user_path(user))
+      expect(flash[:alert]).to be
+    end
   end
 
   context 'Anon no access' do
